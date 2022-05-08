1673d41a206676793b012353c3617d21d07eb45e
M. M. Kabid Hasan 2019331053 <kabidhasan34@gmail.com>
2022-05-09 02:06:35 +0600

    Open File Operation Initiated
    I initiated a very naive and demo open image button just for operational purpose. It opens a file explorer window. We shall decide the perfect UI together  once we get the operations working.

--- src/main/java/com/pixelproteam/pixelpro/HelloApplication.java
+++ src/main/java/com/pixelproteam/pixelpro/HelloApplication.java
@@ -2,6 +2,7 @@ package com.pixelproteam.pixelpro;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/pixelproteam/pixelpro/HelloController.java
+++ src/main/java/com/pixelproteam/pixelpro/HelloController.java
@@ -1,7 +1,11 @@
 package com.pixelproteam.pixelpro;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
     @FXML
@@ -10,5 +14,12 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    private void clickOpenImageButton(ActionEvent e){
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+        System.out.println(selectedFile.getAbsolutePath() );
     }
 }
