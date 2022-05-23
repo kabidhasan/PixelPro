816c73591203e9f59b2c477f9cfda2349bdf7107
M. M. Kabid Hasan 2019331053 <kabidhasan34@gmail.com>
2022-05-23 22:50:47 +0600

    Configured OpenCV and tried to enhance brightness

--- src/main/java/com/pixelproteam/pixelpro/HelloController.java
+++ src/main/java/com/pixelproteam/pixelpro/HelloController.java
@@ -13,7 +13,21 @@ import javafx.stage.FileChooser;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+import org.opencv.core.Core;
+import org.opencv.core.*;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgcodecs.Imgcodecs.*;
+import org.opencv.highgui.HighGui;
+
+import javax.imageio.ImageIO;
 
 public class HelloController {
     @FXML
@@ -25,6 +39,8 @@ public class HelloController {
 
     boolean isImageOpened = false;
 
+    File selectedFile = null;
+
 
     @FXML
     public void clickOpenImageButton(ActionEvent e) {
@@ -32,11 +48,13 @@ public class HelloController {
         fileChooser.getExtensionFilters().addAll(
                 new FileChooser.ExtensionFilter("JPEG Files", "*.png", "*.jpg", "*.jpeg")
         );
-        File selectedFile = fileChooser.showOpenDialog(null);
+
+        selectedFile = fileChooser.showOpenDialog(null);
         System.out.println(selectedFile.getAbsolutePath());
 
         image = new Image(selectedFile.toURI().toString());
         imageView.setImage(image);
+
         isImageOpened = true;
 
         imageView.fitWidthProperty().bind(pane.widthProperty());
@@ -74,13 +92,13 @@ public class HelloController {
                 bufferedImage.setRGB(x, y, p);
             }
         }
-        System.out.println("Success");
-        image = SwingFXUtils.toFXImage(bufferedImage,null);
+        System.out.println("Success1");
+        image = SwingFXUtils.toFXImage(bufferedImage, null);
         imageView.setImage(image);
     }
 
     @FXML
-    private  void clickSepiaFilter (ActionEvent e){
+    private void clickSepiaFilter(ActionEvent e) {
         if (isImageOpened == false) {
             return;
         }
@@ -97,12 +115,12 @@ public class HelloController {
                 int G = (p >> 8) & 0xff;
                 int B = p & 0xff;
 
-                int newRed = (int)(0.393 * R + 0.769 * G
-                                   + 0.189 * B);
-                int newGreen = (int)(0.349 * R + 0.686 * G
-                                     + 0.168 * B);
-                int newBlue = (int)(0.272 * R + 0.534 * G
-                                    + 0.131 * B);
+                int newRed = (int) (0.393 * R + 0.769 * G
+                        + 0.189 * B);
+                int newGreen = (int) (0.349 * R + 0.686 * G
+                        + 0.168 * B);
+                int newBlue = (int) (0.272 * R + 0.534 * G
+                        + 0.131 * B);
 
                 if (newRed > 255)
                     R = 255;
@@ -124,8 +142,142 @@ public class HelloController {
                 bufferedImage.setRGB(x, y, p);
             }
         }
-        System.out.println("Success");
+        System.out.println("Success2");
+        image = SwingFXUtils.toFXImage(bufferedImage, null);
+        imageView.setImage(image);
+    }
+
+    @FXML
+    private void clickDeemedFilter(ActionEvent e) {
+        if (isImageOpened == false) {
+            return;
+        }
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+        BufferedImage bufferedImage = SwingFXUtils.fromFXImage(image, null);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = bufferedImage.getRGB(x, y);
+                int a = (p >> 24) & 0xff;
+                int R = (p >> 16) & 0xff;
+                int G = (p >> 8) & 0xff;
+                int B = p & 0xff;
+
+                int newRed = (int) (1 * R);
+                int newGreen = (int) (0.349 * R + 0.686 * G
+                        + 0.168 * B);
+                int newBlue = (int) (0.272 * R + 0.534 * G
+                        + 0.131 * B);
+
+                if (newRed > 255)
+                    R = 255;
+                else
+                    R = newRed;
+
+                if (newGreen > 255)
+                    G = 255;
+                else
+                    G = newGreen;
+
+                if (newBlue > 255)
+                    B = 255;
+                else
+                    B = newBlue;
+
+                p = (a << 24) | (R << 16) | (G << 8) | B;
+
+                bufferedImage.setRGB(x, y, p);
+            }
+        }
+        System.out.println("Success3");
+        image = SwingFXUtils.toFXImage(bufferedImage, null);
+        imageView.setImage(image);
+
+    }
+
+
+
+
+
+    public static BufferedImage Mat2BufferedImage(Mat mat) throws IOException {
+        //Encoding the image
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", mat, matOfByte);
+        //Storing the encoded Mat in a byte array
+        byte[] byteArray = matOfByte.toArray();
+        //Preparing the Buffered Image
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage bufImage = ImageIO.read(in);
+        return bufImage;
+    }
+//
+//    @FXML
+//    private void clickBrightness (ActionEvent e) throws IOException {
+//        int width;
+//        int height;
+//        double alpha = 1;
+//        double beta = 50;
+//
+//        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+//
+//        // Getting input image by
+//        // creating object of Mat class from local
+//        // directory
+//        Mat source = Imgcodecs.imread( selectedFile.toURI().toString());
+//
+//        Mat destination
+//                = new Mat(source.rows(), source.cols(),
+//                source.type());
+//
+//        // Applying brightness enhancement
+//        source.convertTo(destination, -1, alpha, beta);
+//
+//        BufferedImage bufferedImage = Mat2BufferedImage.Mat2BufferedImage (destination);
+//
+//        image = SwingFXUtils.toFXImage(bufferedImage,null);
+//        imageView.setImage(image);
+//
+//    }
+
+
+    private byte saturate(double val) {
+        int iVal = (int) Math.round(val);
+        iVal = iVal > 255 ? 255 : (iVal < 0 ? 0 : iVal);
+        return (byte) iVal;
+    }
+
+    @FXML
+    private void clickBrightness1(ActionEvent e) {
+        double alpha = 1.1;
+        double beta = 0;
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat img = Imgcodecs.imread(selectedFile.getAbsolutePath());
+        Mat newImage = Mat.zeros(img.size(), img.type());
+        byte[] imageData = new byte[(int) (img.total() * img.channels())];
+        img.get(0, 0, imageData);
+        byte[] newImageData = new byte[(int) (newImage.total() * newImage.channels())];
+        for (int y = 0; y < img.rows(); y++) {
+            for (int x = 0; x < img.cols(); x++) {
+                for (int c = 0; c < img.channels(); c++) {
+                    double pixelValue = imageData[(y * img.cols() + x) * img.channels() + c];
+                    pixelValue = pixelValue < 0 ? pixelValue + 256 : pixelValue;
+                    newImageData[(y * img.cols() + x) * img.channels() + c]
+                            = saturate(alpha * pixelValue + beta);
+                }
+            }
+        }
+        System.out.println("SUCCESS4");
+
+        BufferedImage bufferedImage = null;
+        try {
+            bufferedImage = Mat2BufferedImage (newImage);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
         image = SwingFXUtils.toFXImage(bufferedImage,null);
         imageView.setImage(image);
     }
+
+
 }
