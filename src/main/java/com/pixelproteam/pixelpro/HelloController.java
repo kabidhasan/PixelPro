e7465afb635a95214f91ee68074d527e34ed06e9
M. M. Kabid Hasan 2019331053 <kabidhasan34@gmail.com>
2022-08-07 05:48:05 +0600

    Blending Configured

--- src/main/java/com/pixelproteam/pixelpro/HelloController.java
+++ src/main/java/com/pixelproteam/pixelpro/HelloController.java
@@ -462,7 +462,7 @@ public class HelloController {
 
     @FXML
     public void mirrorVertical(ActionEvent e) {
-        
+
         BufferedImage simg = SwingFXUtils.fromFXImage(image, null);
 
         int width = simg.getWidth();
@@ -517,4 +517,52 @@ public class HelloController {
 
         imageView.setRotate(angle);
     }
+
+    public static BufferedImage scale(BufferedImage src, int w, int h)
+    {
+        BufferedImage img =
+                new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        int x, y;
+        int ww = src.getWidth();
+        int hh = src.getHeight();
+        int[] ys = new int[h];
+        for (y = 0; y < h; y++)
+            ys[y] = y * hh / h;
+        for (x = 0; x < w; x++) {
+            int newX = x * ww / w;
+            for (y = 0; y < h; y++) {
+                int col = src.getRGB(newX, ys[y]);
+                img.setRGB(x, y, col);
+            }
+        }
+        return img;
+    }
+    @FXML
+    public void blend(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPEG Files", "*.png", "*.jpg", "*.jpeg")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+        System.out.println(selectedFile.getAbsolutePath());
+        Image second = new Image(selectedFile.toURI().toString());
+        BufferedImage base = SwingFXUtils.fromFXImage(image, null);
+        BufferedImage overlay = SwingFXUtils.fromFXImage(second, null);
+        int w = Math.max(base.getWidth(), overlay.getWidth());
+        int h = Math.max(base.getHeight(), overlay.getHeight());
+        base = scale(base,w,h);
+        overlay = scale(overlay,w,h);
+        BufferedImage combined = new BufferedImage(w, h,BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = combined.createGraphics();
+        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f));
+        g.drawImage(base, 0, 0, null);
+        g.drawImage(overlay, 0, 0, null);
+        g.dispose();
+        StackMaintain();
+        image = SwingFXUtils.toFXImage(combined, null);
+        imageView.setImage(image);
+        
+    }
+
 }
